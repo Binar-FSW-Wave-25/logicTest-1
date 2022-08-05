db9c01a001de676f4cdc740bbe43212aef3d7c70
FebrianAditya <febrian.aditya@student.upi.edu>
2022-08-05 14:51:15 +0700

    initialize Soal

---
+++ index.js
@@ -0,0 +1,9 @@
+// Soal nomor 1
+/*
+* Buatlah sebuah loop untuk mengecek setiap 3 angka dari 1-100, lalu cetak angka tersebut dan menuliskan apakah bilangan tersebut ganjil atau genap.
+*/
+
+// Soal nomor 2
+/*
+* Buatlah sebuah loop untuk mengecek setiap 5 angka dari 50-200, lalu cetak angka tersebut dan tuliskan apakah bilangan tersebut merupakan faktor perkalian 3 atau bukan.
+*/
