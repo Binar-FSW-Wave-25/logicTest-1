9cb07d0bf144b14817a262f59959703b19dc4102
Al <alvian.febriansyach2@gmail.com>
2022-08-05 20:21:40 +0700

    Initial init

--- index.js
+++ index.js
@@ -7,3 +7,25 @@
 /*
 * Buatlah sebuah loop untuk mengecek setiap 5 angka dari 50-200, lalu cetak angka tersebut dan tuliskan apakah bilangan tersebut merupakan faktor perkalian 3 atau bukan.
 */
+
+// Jawaban - Alvian Febriansyach
+
+// Jawaban nomor 1
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log (i + " adalah angka genap!");
+    }
+    else {
+        console.log (i + " adalah angka ganjil!");
+    }
+}
+
+// Jawaban nomor 2 
+for (let i = 50; i <= 200; i++) {
+    if (i % 3 === 0) {
+        console.log (i + " merupakan faktor perkalian 3");
+    }
+    else {
+        console.log (i + " bukan faktor perkalian 3")
+    }
+}
