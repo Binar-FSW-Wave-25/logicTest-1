af75068f82e3aa9066eb5e3724eeb7aa05df0dbf
BangZulk <zulkarnain.plnwilbabel@gmail.com>
2022-08-08 12:01:19 +0700

    Jawab Soal

---
+++ jawab.js
@@ -0,0 +1,27 @@
+
+// Soal nomor 1
+/*
+* Buatlah sebuah loop untuk mengecek setiap 3 angka dari 1-100, lalu cetak angka tersebut dan menuliskan apakah bilangan tersebut ganjil atau genap.
+*/
+
+
+for (let i=1; i <= 100; i++) {
+    if (i % 2 !== 0){
+        console.log(i+ " ==> angka ganjil")
+    }
+    else {
+        console.log(i+ " ==> angka genap")
+    }
+}
+
+
+// Soal nomor 2
+/*
+* Buatlah sebuah loop untuk mengecek setiap 5 angka dari 50-200, lalu cetak angka tersebut dan tuliskan apakah bilangan tersebut merupakan faktor perkalian 3 atau bukan.
+*/
+
+
+
+
+
+
