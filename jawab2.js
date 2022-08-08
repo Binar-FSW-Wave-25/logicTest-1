16e03effbebaca5c3e61be0ada0aa3d706efb376
BangZulk <zulkarnain.plnwilbabel@gmail.com>
2022-08-08 17:02:19 +0700

    jawaban 2

---
+++ jawab2.js
@@ -0,0 +1,18 @@
+
+
+// Soal nomor 2
+/*
+* Buatlah sebuah loop untuk mengecek setiap 5 angka dari 50-200, lalu cetak angka tersebut dan tuliskan apakah bilangan tersebut merupakan faktor perkalian 3 atau bukan.
+*/
+
+for (let i=50;i<=200;i+=5){
+    if (i%3===0) {
+         console.log(i, " ==> Perkalian 3")
+       } else {
+        console.log (i, " ==> bukan")
+       }
+}
+
+
+
+
